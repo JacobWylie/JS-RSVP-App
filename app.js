35cc96461144fc8e13ed5cdd4ee725638aab7493
JacobWylie <jacobwylie@gmail.com>
2017-03-05 15:42:03 +0100

    created function to handle creating new li

--- app.js
+++ app.js
@@ -2,26 +2,35 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
-form.addEventListener('submit', (e) => {
-	e.preventDefault();
-	const text = input.value;
-	input.value = '';
+// Creates new list item with input text
+function createLI(text) {
 	const li = document.createElement('li');
 	li.textContent = text;
+	// Adds checkbox to list item
 	const label = document.createElement('label');
 	label.textContent = 'Confirmed';
 	const checkbox = document.createElement('input');
 	checkbox.type = 'checkbox';
 	label.appendChild(checkbox);
 	li.appendChild(label);
-
+	// Adds remove button to list item
 	const button = document.createElement('button');
 	button.textContent = 'remove';
 	li.appendChild(button);
+	return li;
+};
 
+// Event handler for form submit
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const text = input.value;
+	input.value = '';
+	// Value of the li created from submit
+	const li = createLI(text);
 	ul.appendChild(li);
 });
 
+// Event Handler  for "Confirmed" checkbox
 ul.addEventListener('change', (e) => {
 	const checkbox = event.target;
 	const checked = checkbox.checked;
@@ -34,6 +43,7 @@ ul.addEventListener('change', (e) => {
 	}
 });
 
+// Event Handler for "remove" button
 ul.addEventListener('click', (e) => {
 	if (e.target.tagName === 'BUTTON') {
 		const li = e.target.parentNode;
